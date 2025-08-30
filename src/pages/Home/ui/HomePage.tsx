5a1cfba54a412c227cc9846fc36af431d842890f
Kishan Kalavadiya <36093612+kishumohi@users.noreply.github.com>
2025-08-30 16:59:32 +0530

    03_UI_Button Create

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router';
 import styles from './HomePage.module.scss';
 import { useTheme } from '@/shared/config';
+import { Button } from '@/shared/ui';
 
 function HomePage() {
     const { toggleTheme } = useTheme();
     return (
         <>
             <h1 className={styles.title}>Home</h1>
-            <button onClick={toggleTheme}>Theme</button>
+            <Button theme='outline' onClick={toggleTheme}>
+                Theme
+            </Button>
             <Link to={'/login'}>Login Page</Link>
         </>
     );
